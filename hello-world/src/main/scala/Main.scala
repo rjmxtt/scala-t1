7d7f652689b95f95e6725a1233e5a0e49f24b3c1
rjm <rjm@temple.home>
2021-02-26 15:22:23 +0000

    query result and count wins on day

--- hello-world/src/main/scala/Main.scala
+++ hello-world/src/main/scala/Main.scala
@@ -1,47 +1,72 @@
 import java.io._
 
-object Demo {
+object Main {
    def main(args: Array[String]) {
-      // val d = new Draw()
-      val s = new System()
+
+    var s = new System() 
+    val random = scala.util.Random
+
+    for ( a <- 1 to 4 ) {
+      s.newDraw(a)
+      for ( b <- 1 to 99999 ) {
+        s.enter( b,  random.nextInt(999) )
+      }
+      s.playDate( a )
+    }
+
+    if (args(0) == "result") {
+      s.result( args(1).toInt , args(2).toInt )
+    }
+
+    if (args(0) == "countWins") {
+      println( s.lottery.get( args(1).toInt ).get.winners.length )
+    }
+
+      // val t = new Test()
+      // val s = 
+      // println(t.setup().lottery)
+
+      // t.setup().result(1, 33)
+
+      // if (args(0) == "result") {
+      //   s.result(args(1), args(2))
+      // }
 
       // s.newDraw(10004)
 
-      s.enter(10,0)
-      s.enter(11,1)
-      s.enter(12,2)
-      s.enter(13,3)
+        // s.enter(10,0)
+        // s.enter(11,1)
+        // s.enter(12,2)
+        // s.enter(13,3)
       
-      s.playToday()
+        // s.playToday() 
 
-      s.result(100003,12)
-
-      // s.newDraw()
-      // d.play
+        // s.result(100003,12)
    }
 }
 
 class System() {
-  var lottery: Map[Int,Draw] = Map(
-    100001 -> new Draw(),
-    100002 -> new Draw(),
-    100003 -> new Draw()
-  )
+  var lottery: Map[Int,Draw] = Map()
 
   def newDraw(date:Int) = {
     lottery += (date -> new Draw())
-    println(lottery)
+    // println(lottery)
   }
   
   def enter(id: Int, num: Int) {
     val t = new Ticket(id,num)
     lottery.last._2.enter(t)
-    println("Draw->"+lottery.last._2.tickets)
+    // println("Draw->"+lottery.last._2.tickets)
   }
 
   def playToday() {
     lottery.last._2.play()
-    println(lottery.last._2.randomInt + " - " + lottery.last._2.winners)
+    // println(lottery.last._2.randomInt + " - " + lottery.last._2.winners)
+  }
+
+  def playDate(date:Int) {
+    lottery.get(date).get.play()
+    // println(date)
   }
 
   def result(date:Int,customer:Int) {
@@ -52,21 +77,17 @@ class System() {
 
 class Draw() {
   var tickets: List[Ticket] = List()
-  var winners: List[Int] = List(8)
+  var winners: List[Int] = List()
 
-  // val date: Int = pdate
   val random = scala.util.Random
   val randomInt: Int = random.nextInt(3)
-  // println(randomInt)
 
   def play() {
     winners = tickets.filter( _.number == randomInt ).map(x => x.customer)
-    // println(winners)
   }
 
   def enter(pticket: Ticket) {
     tickets = pticket :: tickets
-    // println(tickets)
   }
 }
 
@@ -74,3 +95,22 @@ class Ticket(
   val customer: Int,
   val number: Int
 )
+
+// class Test() {
+//   def setup() {
+
+//     var s = new System() 
+//     val random = scala.util.Random
+
+//     for ( a <- 1 to 4 ) {
+//       s.newDraw(a)
+//       for ( b <- 1 to 99999 ) {
+//         s.enter( b,  random.nextInt(999) )
+//       }
+//       s.playDate( a )
+//     }
+
+//     return s
+
+//   }
+// }
